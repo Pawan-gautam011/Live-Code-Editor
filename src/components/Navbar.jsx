f0661cdf85ebadfb48615b05f19f457b65f48f89
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-21 23:39:01 +0545

    implementing light mode and dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import logo from '../assets/logo.png'
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+import { CiDark } from "react-icons/ci";
+import { GoSun } from "react-icons/go";
+
 const Navbar = () => {
+  
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'rgb(17,24,39)';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'rgb(255,255,255)';
+    }
+  };
+
   return (
-    <div
-    className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
-        <div className='left'>
-            <div className='logo font-bold text-2xl text-white text-center'>
-                <img className='w-44' src={logo} alt="Logo" />
-
-            </div>
-        </div>
-
+    <div className={`main flex flex-wrap justify-between items-center px-4 py-4 ${mode === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
+      <div className='logo font-bold text-2xl flex items-center w-auto'>
+        <img className='w-44' src={logo} alt="Logo" />
+      </div>
+      <div className='icons flex justify-end items-center space-x-4 w-auto'>
+        {mode === 'light' ? (
+          <CiDark onClick={toggleMode} className='text-2xl cursor-pointer' />
+        ) : (
+          <GoSun onClick={toggleMode} className='text-2xl cursor-pointer' />
+        )}
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
