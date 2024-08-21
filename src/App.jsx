de39be7f5563e74e64b022f96441f9433c77d1fe
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-21 15:28:07 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+  
+      <h1 className=' text-3xl font-bold grid place-content-center h-screen'>Welcome To the the code Editior </h1>
+
+    </>
+  )
+}
+
+export default App
