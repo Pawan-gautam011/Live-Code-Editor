e7e6c7f942de4814ccac2587949bedf7bcad2bde
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-21 16:29:40 +0545

    Adding live code editor with HTML, CSS, and JavaScript integration using CodeMirror and iframe for real-time preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,82 @@
-
+import { useCallback, useState } from 'react';
+import Navbar from './components/Navbar';
+import CodeMirror from '@uiw/react-codemirror';
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import { javascript } from '@codemirror/lang-javascript';
+import Results from './components/Results';
 
 function App() {
+  const [htmledit, setHtmlEdit] = useState('');
+  const [cssedit, setCssEdit] = useState('');
+  const [jsedit, setJsEdit] = useState('');
+
+  const onChangeHtml = useCallback((value) => {
+    setHtmlEdit(value);
+  }, []);
+
+  const onChangeCss = useCallback((value) => {
+    setCssEdit(value);
+  }, []);
+
+  const onChangeJs = useCallback((value) => {
+    setJsEdit(value);
+  }, []);
+
+  const srcCode = `
+    <html>
+    <body>${htmledit}</body>
+    <style>${cssedit}</style>
+    <script>${jsedit}</script>
+    </html>
+  `;
 
   return (
     <>
-  
-      <h1 className=' text-3xl font-bold grid place-content-center h-screen'>Welcome To the the code Editior </h1>
+      <Navbar />
+      <div className='p-2'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
+          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
+            <h2 className='text-lg font-semibold mb-2 text-white'>HTML</h2>
+            <CodeMirror
+              className='text-xl border-gray-700 border'
+              value={htmledit}
+              height='342px'
+              theme='dark'
+              extensions={[html(true)]}
+              onChange={onChangeHtml}
+            />
+          </div>
 
+          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
+            <h2 className='text-lg font-semibold mb-2 text-white'>CSS</h2>
+            <CodeMirror
+              className='text-xl border-gray-700 border'
+              value={cssedit}
+              height='342px'
+              theme='dark'
+              extensions={[css(true)]}
+              onChange={onChangeCss}
+            />
+          </div>
+
+          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
+            <h2 className='text-lg font-semibold mb-2 text-white'>JavaScript</h2>
+            <CodeMirror
+              className='text-xl border-gray-700 border'
+              value={jsedit}
+              height='342px'
+              theme='dark'
+              extensions={[javascript(true)]}
+              onChange={onChangeJs}
+            />
+          </div>
+
+          <Results srcCode={srcCode} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
