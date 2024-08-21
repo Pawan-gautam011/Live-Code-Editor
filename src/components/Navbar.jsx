155a4d841238e5c1925f62e460f8055eaa663f3d
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-08-21 23:47:17 +0545

    Integrating theme toggle with local storage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
 import { CiDark } from "react-icons/ci";
 import { GoSun } from "react-icons/go";
 
 const Navbar = () => {
-  
   const [mode, setMode] = useState('light');
 
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark');
-      document.body.style.backgroundColor = 'rgb(17,24,39)';
-    } else {
-      setMode('light');
-      document.body.style.backgroundColor = 'rgb(255,255,255)';
+  useEffect(() => {
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode) {
+      setMode(savedMode);
+      document.body.style.backgroundColor = savedMode === 'light' ? 'rgb(255,255,255)' : 'rgb(17,24,39)';
     }
+  }, []);
+
+  const toggleMode = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light';
+    setMode(newMode);
+    document.body.style.backgroundColor = newMode === 'light' ? 'rgb(255,255,255)' : 'rgb(17,24,39)';
+    localStorage.setItem('mode', newMode); 
   };
 
   return (
-    <div className={`main flex flex-wrap justify-between items-center px-4 py-4 ${mode === 'light' ? 'bg-white text-black' : 'bg-gray-800 text-white'}`}>
+    <div className={`main flex flex-wrap justify-between items-center px-4 py-4  ${mode === 'light' ? 'bg-white text-black border' : 'bg-gray-800 text-white'}`}>
       <div className='logo font-bold text-2xl flex items-center w-auto'>
         <img className='w-44' src={logo} alt="Logo" />
       </div>
